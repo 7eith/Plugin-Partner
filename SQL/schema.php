5f3477b8bf221907e5a54b8eed41fa0a04e31e51
Alexis M <iamsnkh@pm.me>
2019-03-27 14:22:13 +0100

    :sparkles: Create plugin's Schema

--- SQL/schema.php
+++ SQL/schema.php
@@ -1,11 +1,36 @@
 <?php
 /* **************************************************************************** *
- *  
+ *
  * schema.php
  *
- * by: Snkh <dev@snkh.me> 
- * 
- * Created: 24/03/2019 21:28 by Snkh
+ * by: Snkh <dev@snkh.me>
+ *
+ * Created: 24/03/2019 18:48 by Snkh
  * Under private Copyright, all rights reserved to Snkh.
  *
  **************************************************************************** */
+
+class PartnerAppSchema extends CakeSchema {
+
+    public $file = 'schema.php';
+
+    public function before($event = array()) {
+        return true;
+    }
+
+    public function after($event = array()) {}
+
+    public $partner__requests = array(
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 20, 'unsigned' => false, 'key' => 'primary'),
+        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+        'link' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'description' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'response' => array('type' => 'boolean', 'null' => false, 'default' => 0),
+        'admin_response' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id', 'unique' => 1)
+        ),
+        'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+    );
+}
