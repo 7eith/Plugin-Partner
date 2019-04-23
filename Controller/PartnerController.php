d011932c7d048368c0b99a427ed971a7cec49504
Alexis M <iamsnkh@pm.me>
2019-04-24 01:41:28 +0200

    :sparkles: create controller rest functions

--- Controller/PartnerController.php
+++ Controller/PartnerController.php
@@ -9,19 +9,45 @@
  * Under private Copyright, all rights reserved to Snkh.
  *
  **************************************************************************** */
-
- /*
-   INDEX, 
-   ADD, from user, id, link, text 
-   DELETE, admin, id, 
-   ANSWER, id, text, 
-   SHOW id
-  */ 
-
  class PartnerController extends AppController { 
 
     /** 
-     * Admin Functions, send all PartnerRequests 
+     * Create PartnerRequest and store it.
+     */
+
+     public function create() {
+        if(!$this->isConnected) 
+            throw new ForbiddenException();
+
+        if(!$this->request->is('ajax')) 
+            return $this->response->body(json_encode(array('statut' => false, 'msg' => $this->Lang->get('ERROR__BAD_REQUEST'))));
+
+        $this->loadModel('Partner.PartnerRequest');
+        $this->response->type('json');
+        $this->autoRender = false;
+
+        // has already try 
+        if($this->PartnerRequest->find('count', ['conditions' => ['user_id' => $this->User->getKey('id')]]) != 0) 
+            return $this->response->body(json_encode(array('statut' => false, 'msg' => $this->Lang->get('PARTNER__HAS_ALREADY_ASKED'))));
+        
+        if (!empty($this->request->data['link']) AND !empty($this->request->data['subs']) AND !empty($this->request->data['description'])) {
+
+            if(!is_numeric($this->request->data['subs'])) 
+                return $this->response->body(json_encode(array('statut' => false, 'msg' => $this->Lang->get('PARTNER__ERROR_NUMBER'))));
+        
+            $this->PartnerRequest->read(null, null);
+            $this->PartnerRequest->set($this->request->data);
+            $this->PartnerRequest->set(array('user_id' => $this->User->getKey('id')));
+            $this->PartnerRequest->save();
+
+            $this->response->body(json_encode(array('statut' => true, 'msg' => $this->Lang->get('PARTNER__REQUEST_OK'))));
+        } else { 
+            $this->response->body(json_encode(array('statut' => false, 'msg' => $this->Lang->get('ERROR__FILL_ALL_FIELDS'))));
+        }
+     }
+
+    /** 
+     * Index all requests
      */
 
     public function admin_index(){
@@ -30,39 +56,82 @@
         $this->loadModel('Partner.PartnerRequest');
 
         $usersToFind = array();
-        $partnerRequests = $this->PartnerRequest->find('all');
+        $partnerRequests = $this->PartnerRequest->find('all', array('conditions' => array('response' => null)));
         foreach ($partnerRequests as $key => $value) {
             $usersToFind[] = $value['PartnerRequest']['user_id'];
         }
-        // On récupère tous les utilisateurs pour afficher leur pseudo
+
         $usersByID = array();
         $findUsers = $this->User->find('all', array('conditions' => array('id' => $usersToFind)));
         foreach ($findUsers as $key => $value) {
             $usersByID[$value['User']['id']] = $value['User']['pseudo'];
         }
-        // send to vue 
+        
         $this->set(compact('partnerRequests', 'usersByID'));
     }
 
-    /** 
-     * Delete PartnerRequests, only admin 
-     * params: id of PartnerRequest
-     */
+    public function admin_answer() { 
+        $this->layout = 'admin';
+        $this->set('title_for_layout', 'Partner');
+        $this->loadModel('Partner.PartnerRequest');
 
-    public function admin_delete($id) { 
-        
+        if (isset($this->request->params['id'])) {
+            $id = $this->request->params['id'];
+            $partnerRequest = $this->PartnerRequest->find('first', ['conditions' => ['id' => $id]]);
+            $partnerRequester = $this->User->find('first', ['conditions' => ['id' => $partnerRequest['PartnerRequest']['user_id']]]);
+
+            if (empty($partnerRequest))
+                throw new NotFoundException();
+
+            $this->set('partnerRequest', $partnerRequest['PartnerRequest']);
+            $this->set('partnerRequester', $partnerRequester['User']);
+        }
+
+        else
+            $id = null;
     }
 
-    public function admin_answer($id, $text) { 
-        
+    public function admin_accept() {
+        $this->layout = null;
+        $this->autoRender = false;
+        $this->loadModel('Partner.PartnerRequest');
+        $this->loadModel('Notification');
+
+        if($this->request->is('ajax')) {
+            if (isset($this->request->params['id'])) {
+                $id = $this->request->params['id'];
+                
+                $partnerRequest = $this->PartnerRequest->find('first', ['conditions' => ['id' => $id]]);
+                $partnerRequest['PartnerRequest']['response'] = true; 
+                $this->PartnerRequest->save($partnerRequest);
+
+                $this->Notification->setToUser($this->Lang->get('PARTNER__NOTIFICATION_ACCEPT'), $partnerRequest['PartnerRequest']['user_id'], $this->User->getKey('id'), 'user');
+
+                $this->response->type('json');
+                $this->response->body(json_encode(array('statut' => true, 'msg' => $this->Lang->get('PARTNER__ANSWERED_ACCEPT'))));
+            }
+        } 
     }
 
-    public function admin_show($id) { 
-        
+    public function admin_refuse() { 
+        $this->layout = null;
+        $this->autoRender = false;
+        $this->loadModel('Partner.PartnerRequest');
+        $this->loadModel('Notification');
+
+        if($this->request->is('ajax')) {
+            if (isset($this->request->params['id'])) {
+                $id = $this->request->params['id'];
+                
+                $partnerRequest = $this->PartnerRequest->find('first', ['conditions' => ['id' => $id]]);
+                $partnerRequest['PartnerRequest']['response'] = false; 
+                $this->PartnerRequest->save($partnerRequest);
+
+                $this->Notification->setToUser($this->Lang->get('PARTNER__NOTIFICATION_REFUSE'), $partnerRequest['PartnerRequest']['user_id'], $this->User->getKey('id'), 'user');
+
+                $this->response->type('json');
+                $this->response->body(json_encode(array('statut' => true, 'msg' => $this->Lang->get('PARTNER__ANSWERED_REFUSE'))));
+            }
+        } 
     }
-
-    public function add($user, $link, $text) { 
-
-    }
-
  }
